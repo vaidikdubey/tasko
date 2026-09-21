626c57d8e561e204379bf7d27350d693e4802c82
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-09-21 09:03:49 +0000

    feat: add dependencies vector to Task struct

--- src/task.h
+++ src/task.h
@@ -1,6 +1,7 @@
 #pragma once // Telling compiler to read this file only once even if multiple imports are +nt
 
 #include <string>
+#include <vector>
 
 struct Task
 {
@@ -8,6 +9,7 @@ struct Task
     std::string name;
     int priority;
     bool completed;
+    std::vector<int> dependencies;
 
     Task(int id, std::string name, int priority);
 };
