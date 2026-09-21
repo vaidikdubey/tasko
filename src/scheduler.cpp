de7c6e5bca8eaee6809e46754a3e0ffdd34783d5
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-09-21 09:21:24 +0000

    feat: add dependency graph with DFS cycle detection and task completion

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -1,4 +1,5 @@
 #include "scheduler.h"
+#include <iostream>
 
 using namespace std;
 
@@ -11,17 +12,105 @@ void Scheduler::addTask(string name, int priority) // O(logN)
 {
     Task task(nextId++, name, priority);
     tasks.push(task);
+    taskMap[task.id] = task;
+    adjList[task.id] = {};
 }
 
-vector<Task> Scheduler::getSchedule() // O(NlogN)
+// Colors {
+//     White(0) — not visited;
+//     Gray(1) — currently being visited(in the DFS stack);
+//     Black(2) — fully processed;
+// }
+
+bool Scheduler::hasCycle(int taskId, unordered_map<int, int> &color)
+{
+    color[taskId] = 1;
+
+    for (int depId : adjList[taskId])
+    {
+        if (color[depId] == 1)
+            return true;
+
+        if (color[depId] == 0 && hasCycle(depId, color))
+            return true;
+    }
+
+    color[taskId] = 2;
+
+    return false;
+}
+
+bool Scheduler::addDependency(int taskId, int dependsOnId)
+{
+    if (taskMap.find(taskId) == taskMap.end())
+        return false;
+    if (taskMap.find(dependsOnId) == taskMap.end())
+        return false;
+
+    adjList[taskId].push_back(dependsOnId);
+
+    unordered_map<int, int> color;
+
+    for (auto &pair : taskMap)
+    {
+        color[pair.first] = 0;
+    }
+
+    if (hasCycle(taskId, color))
+    {
+        adjList[taskId].pop_back();
+        return false;
+    }
+
+    taskMap[taskId].dependencies.push_back(dependsOnId);
+
+    return true;
+}
+
+bool Scheduler::completeTask(int taskId)
+{
+    if (taskMap.find(taskId) == taskMap.end())
+        return false;
+
+    for (int depId : adjList[taskId])
+    {
+        if (!taskMap[depId].completed)
+            return false;
+    }
+
+    taskMap[taskId].completed = true;
+
+    return true;
+}
+
+vector<Task> Scheduler::getSchedule()
 {
     priority_queue<Task, vector<Task>, TaskComparator> temp = tasks;
     vector<Task> sorted;
 
-    while (!temp.empty()) // O(NlogN -> due to popping N elements)
+    while (!temp.empty())
     {
-        sorted.push_back(temp.top()); // O(1)
-        temp.pop();                   // O(logN)
+        Task t = temp.top();
+        temp.pop();
+
+        t.completed = taskMap[t.id].completed;
+        t.dependencies = taskMap[t.id].dependencies;
+
+        bool depsCompleted = true;
+
+        for (int depId : t.dependencies)
+        {
+            if (!taskMap[depId].completed)
+            {
+                depsCompleted = false;
+                break;
+            }
+        }
+
+        if (!t.completed && !depsCompleted)
+        {
+            sorted.push_back(t);
+        }
     }
 
     return sorted;
